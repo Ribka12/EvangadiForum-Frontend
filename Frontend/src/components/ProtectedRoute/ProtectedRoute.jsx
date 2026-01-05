dbc21bcb74b30a5111269683d21331743d284eb4
Ribka Mengiste <167540456+Ribka12@users.noreply.github.com>
2026-01-05 19:37:48 +0300

    Init structure

---
+++ Frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  // If no token → redirect to login
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  // Otherwise render the protected page
+  return children;
+};
+export default ProtectedRoute;
