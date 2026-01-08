0b86200149e140cb4626b2deba5361ca1b78bf14
AlexB <alexb@example.com>
2026-01-08 19:51:27 +0300

    Update Home and necessary compoponetes to do it

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,3 @@
-// import React, { useState } from "react";
-// import { Routes, Route, Navigate } from "react-router-dom";
-// import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
-// import Home from "./pages/Home-page/Home";
-// import Login from "./components/Login/Login";
-// import Context from "./context";
-
-// function App() {
-//   const [user, setUser] = useState(null);
-
-//   return (
-//     <Context.Provider value={{ user, setUser }}>
-//       <Routes>
-//         <Route path="/" element={<Navigate to="/home" replace />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route
-//           path="/home"
-//           element={
-//             <ProtectedRoute>
-//               <Home />
-//             </ProtectedRoute>
-//           }
-//         />
-//       </Routes>
-//     </Context.Provider>
-//   );
-// }
-
-// export default App;
 
 
 import React, { useState } from "react";
